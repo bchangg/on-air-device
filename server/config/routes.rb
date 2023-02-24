01b4792178922d6c9e959f83df984148873bf5b6
Brian Chang <changbhc@gmail.com>
2023-02-24 13:58:34 -0800

    Add simple home route/page

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#hello"
+
   namespace :api do
     namespace 'v1' do
       scope 'mqtt' do
