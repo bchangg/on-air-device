f957897550c90c20d76e87b43694edb5a6a642e7
Brian Chang <changbhc@gmail.com>
2023-03-17 14:48:48 -0700

    Add proof of concept publishing to MQTT broker

--- server/app/controllers/api/v1/mqtt_controller.rb
+++ server/app/controllers/api/v1/mqtt_controller.rb
@@ -1,5 +1,23 @@
 class Api::V1::MqttController < ApplicationController
   def publish
-    render plain: 'publish some info to mqtt'
+    MQTT::Client.connect(
+      host: ENV['MQTT_CLUSTER_URL'],
+      username: ENV['MQTT_USERNAME'],
+      password: ENV['MQTT_PASSWORD'],
+      ssl: true
+    ) do |client|
+      client.publish(ENV['MQTT_TOPIC'], ENV['MQTT_MESSAGE'])
+    end
+
+    render json: {
+      success: true,
+      topic: ENV['MQTT_TOPIC'], 
+      message: ENV['MQTT_MESSAGE']
+    }, status: :ok
+  rescue => e
+    render json: {
+      success: false,
+      message: e
+    }, status: :not_acceptable
   end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace 'v1' do
       scope 'mqtt' do
-        get 'publish', to: 'mqtt#publish'
+        post 'publish', to: 'mqtt#publish'
       end
     end
   end
